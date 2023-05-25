10b3e94f50a244e545a31a8dc304b512643202fe
Ailson Cardoso <95575300+ailsacs@users.noreply.github.com>
2023-05-25 08:11:45 -0300

    improving infra from main, added bucket resource

--- main.tf
+++ main.tf
@@ -11,6 +11,47 @@ resource "aws_instance" "dev" {
   tags = {
     Name = "dev_${count.index}"
   }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev_s3" {
+  ami = "ami-026c8acd92418196b"
+  instance_type = "t2.micro"
+  key_name = "challenges_aws"
+  tags = {
+    Name = "dev_s3"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = ["aws_s3_bucket.dev_s3"]
 }
 
 
+
+# creating security group.
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["single/range-ip"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
+
+resource "aws_s3_bucket" "dev_s3" {
+  bucket = "challenges-s3"
+  acl    = "private"
+
+  tags = {
+    Name = "challenges-s3"
+  }
+}
